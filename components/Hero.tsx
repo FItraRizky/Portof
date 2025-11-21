37bfff34acf249b1721c9bfe19eb636557f0849b
fitrarizky <orian3969@gmail.com>
2025-11-21 15:51:15 +0700

    mouse

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -137,28 +137,6 @@ export default function Hero() {
           </div>
         </motion.div>
       </div>
-
-      {/* Scroll Indicator - Absolute positioned at bottom */}
-      <motion.div
-        className="absolute bottom-10 left-1/2 -translate-x-1/2"
-        initial={{ opacity: 0, y: -20 }}
-        animate={{ opacity: 1, y: 0 }}
-        transition={{ delay: 1.5, duration: 1 }}
-      >
-        <div className="w-6 h-10 border-2 border-muted/30 rounded-full flex justify-center p-2">
-          <motion.div
-            className="w-1.5 h-1.5 bg-muted rounded-full"
-            animate={{
-              y: [0, 12, 0],
-            }}
-            transition={{
-              duration: 1.5,
-              repeat: Infinity,
-              ease: "easeInOut",
-            }}
-          />
-        </div>
-      </motion.div>
     </section>
   );
 }
